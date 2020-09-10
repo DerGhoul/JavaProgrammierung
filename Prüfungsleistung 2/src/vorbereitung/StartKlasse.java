63067ea5a593bfb4d6a7ca813337011a3b98bc10
MalteHensel <malte_hensel@outlook.de>
2020-09-10 16:30:20 +0200

    bley

---
+++ Prüfungsleistung 2/src/vorbereitung/StartKlasse.java
@@ -0,0 +1,24 @@
+package vorbereitung;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartKlasse extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("View1.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Demo 2.10");
+		primaryStage.setScene(scene);
+		primaryStage.show();		
+	}
+	
+}
