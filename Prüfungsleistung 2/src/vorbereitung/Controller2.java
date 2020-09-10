d59da80d9949edb170b246aa45ad735c92fb1c3f
DerGhoul1 <56272358+DerGhoul1@users.noreply.github.com>
2020-09-10 15:11:55 +0200

    Controller2

---
+++ Prüfungsleistung 2/src/vorbereitung/Controller2.java
@@ -0,0 +1,16 @@
+package vorbereitung;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller2 {
+
+	@FXML 
+	Label label4;
+	
+	@FXML
+	Label label5;
+	
+	
+	
+}
