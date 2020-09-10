2f82eaa3a77640822ae24d8c45992dfb80462be2
MalteHensel <malte_hensel@outlook.de>
2020-09-10 17:04:34 +0200

    controller2 backButton

--- Prüfungsleistung 2/src/vorbereitung/Controller2.java
+++ Prüfungsleistung 2/src/vorbereitung/Controller2.java
@@ -1,7 +1,13 @@
 package vorbereitung;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Controller2 {
 
@@ -10,6 +16,21 @@ public class Controller2 {
 	
 	@FXML
 	Label label5;
+	
+	@FXML
+	Button backButton;
+	
+	@FXML
+	public void backToInputView(ActionEvent goBack) throws Exception {
+		Scene oldScene = backButton.getScene();
+		Stage stage = (Stage) oldScene.getWindow();
+		
+		Parent root = FXMLLoader.load(getClass().getResource("View1.fxml"));
+		Scene newScene = new Scene(root);
+		stage.setTitle("Ergebnis");
+		stage.setScene(newScene);
+		stage.show();
+	}
 	
 	
 	
