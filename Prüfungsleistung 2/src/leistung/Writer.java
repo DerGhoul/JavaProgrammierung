da282fd1523a1089058ee01c8ad243a9202c4242
DerGhoul1 <56272358+DerGhoul1@users.noreply.github.com>
2020-09-11 13:15:44 +0200

    sdfa

--- Prüfungsleistung 2/src/leistung/Writer.java
+++ Prüfungsleistung 2/src/leistung/Writer.java
@@ -28,9 +28,34 @@ public class Writer {
 				}
 			catch (IOException e) {
 				e.printStackTrace();
-			}
 		}
-	}	
+	}
+	
+	
+	public void read() {
+		Model m =  Model.getInstance();
+		
+		String titel = m.getTitle();
+		String genre = m.getGenre();
+		String yearOfRelease = m.getYearofRelease();
+		String system = m.getSystem();
+		String playedThrough = m.getPlayedThrough()+"";
+		
+	
+		
+		File file = new File("C:/Temp/MyGames.txt");
+	
+		try (FileWriter fw = new FileWriter(file);
+				BufferedWriter bw = new BufferedWriter(fw)){
+			 
+				bw.write(titel + " " + genre + " " + yearOfRelease + " " + system + " " + playedThrough);
+				}
+			catch (IOException e) {
+				e.printStackTrace();
+		}
+	}
+	
+}	
 
 
 
