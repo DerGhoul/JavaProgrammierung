2b44a1693e80f5c6a436eb703e053aa1b3b37317
niklas zenger@accenture.com <niklas.zenger@CPX-QRCTN95A35I.dir.svc.accenture.com>
2020-09-11 12:52:48 +0200

    writer

--- Prüfungsleistung 2/src/leistung/Writer.java
+++ Prüfungsleistung 2/src/leistung/Writer.java
@@ -8,6 +8,12 @@ import java.io.IOException;
 public class Writer {
 	
 	public void write(){
+		String titel = Model.getInstance().getTitel();
+		String genre = Model.getGenre();
+		String yearOfRelease = Model.getYearofRelease();
+		String system = Model.getSystem();
+		String playedThrough = Model.getPlayedThrough();
+		
 	
 		
 		File file = new File("C:/Temp/MyGames.txt");
@@ -15,7 +21,7 @@ public class Writer {
 		try (FileWriter fw = new FileWriter(file);
 				BufferedWriter bw = new BufferedWriter(fw)){
 			 
-				//bw.write(titel + " " + genre + " " + yearOfRelease + " " + system + " " + playedthrough);
+				bw.write(titel + " " + genre + " " + yearOfRelease + " " + system + " " + playedThrough);
 				}
 			catch (IOException e) {
 				e.printStackTrace();
