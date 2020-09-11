c36e5720e617c7c9288112b899d1cc2add03901e
MalteHensel <malte_hensel@outlook.de>
2020-09-11 11:53:58 +0200

    Szenenwechsel im NewGame Fenster

--- Prüfungsleistung 2/src/leistung/NewGameController.java
+++ Prüfungsleistung 2/src/leistung/NewGameController.java
@@ -1,12 +1,20 @@
 package leistung;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 
 public class NewGameController {
 
@@ -26,6 +34,41 @@ public class NewGameController {
 	@FXML HBox buttonPart;
 	@FXML Button newGameEnter;
 	@FXML Button newGameQuit;
+	
+		
+	/*
+	 * Szenenwechsel nach Betätigung des Hinzufügenknopfes
+	 */
+	@FXML public void newGameEnter(ActionEvent event) throws IOException {
+		
+		Scene oldScene = newGameEnter.getScene();
+		Stage stage = (Stage) oldScene.getWindow();
+
+		Parent root = FXMLLoader.load(getClass().getResource("ViewMyGames.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("Meine Spiele");
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+	/*
+	 * Szenenwechsel nach Betätigung des Abbruchknopfes
+	 */
+	@FXML public void newGameQuit(ActionEvent event) throws IOException {
+		
+		Scene oldScene = newGameEnter.getScene();
+		Stage stage = (Stage) oldScene.getWindow();
+
+		Parent root = FXMLLoader.load(getClass().getResource("ViewMyGames.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("Meine Spiele");
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	
 	
 	
 
