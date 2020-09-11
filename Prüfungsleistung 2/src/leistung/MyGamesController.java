50ede0ca5bb5265edc9082194abec7ad8a4dca81
MalteHensel <malte_hensel@outlook.de>
2020-09-11 11:07:36 +0200

    SceneSwitch von MyGAmes auf NewGame

--- Prüfungsleistung 2/src/leistung/MyGamesController.java
+++ Prüfungsleistung 2/src/leistung/MyGamesController.java
@@ -1,12 +1,14 @@
 package leistung;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javax.swing.text.html.Option;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableCell;
@@ -14,12 +16,15 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioMenuItem;
 
@@ -82,7 +87,18 @@ public class MyGamesController implements Initializable {
 
 
 
-	@FXML public void createNewGame(ActionEvent event) {}
+	@FXML public void createNewGame(ActionEvent event) throws IOException {
+		
+		Scene oldScene = newGame.getScene();
+		Stage stage = (Stage) oldScene.getWindow();
+
+		Parent root = FXMLLoader.load(getClass().getResource("ViewNewGame.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("Meine Spiele");
+		stage.setScene(scene);
+		stage.show();
+		
+	}
 
 	 
 	
