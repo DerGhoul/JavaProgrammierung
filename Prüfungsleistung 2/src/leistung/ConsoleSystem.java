8d868cd0752188cfa0009635aa3ab1aaa05d1d68
niklas zenger@accenture.com <niklas.zenger@CPX-QRCTN95A35I.dir.svc.accenture.com>
2020-09-11 09:54:25 +0200

    ConsoleSystem

--- Prüfungsleistung 2/src/leistung/ConsoleSystem.java
+++ Prüfungsleistung 2/src/leistung/ConsoleSystem.java
@@ -3,21 +3,24 @@ package leistung;
 public enum ConsoleSystem {
 	
 	PS4("Playstation 4"),XONE("X Box One"),Switch("Nintendo Switch");
-	
+	/*
+	 * Attribute
+	 */
 	private String description;
+	private String abberivation;
+	/*
+	 * Methoden
+	 */
 	ConsoleSystem(String description) {
 		this.description = description;
+		this.abberivation = description;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
-	
-	private String Abbreviation;
-	ConsoleSystem(String Abbreviation) {
-		this.Abbreviation = Abbreviation;
-	}
-	public String getDescription() {
-		return Abbreviation;
+	public String getAbberivation() {
+		return abberivation;
 	}
 
 
