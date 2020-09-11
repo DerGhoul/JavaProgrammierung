535b41bf8920f77a7666409da989e450d523bcde
MalteHensel <malte_hensel@outlook.de>
2020-09-11 11:16:08 +0200

    NewGameController

--- Prüfungsleistung 2/src/leistung/NewGameController.java
+++ Prüfungsleistung 2/src/leistung/NewGameController.java
@@ -1,5 +1,32 @@
 package leistung;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+
 public class NewGameController {
+
+	@FXML Label newGameTitle;
+	@FXML Label newGameGenre;
+	@FXML Label newGameReleaseDate;
+	@FXML Label newGameSystem;
+	@FXML Label newGameOS;
+	@FXML Label newGameCS;
+	@FXML TextField newGameInputTitle;
+	@FXML ComboBox<Genre> newGameInputGenre;
+	@FXML TextField newGameInputReleaseDate;
+	@FXML RadioButton newGameSystemInputConsole;
+	@FXML RadioButton newGameSystemInputPC;
+	@FXML RadioButton newGameSystemInputMG;
+	@FXML ComboBox<Genre> newGameInputRS;
+	@FXML HBox buttonPart;
+	@FXML Button newGameEnter;
+	@FXML Button newGameQuit;
+	
+	
 
 }
