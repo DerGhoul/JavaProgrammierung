1302b6d3198b8aa4487bfd0408a491dad015538c
DerGhoul1 <56272358+DerGhoul1@users.noreply.github.com>
2020-09-11 10:41:30 +0200

    Edit Game View in Controller

--- Prüfungsleistung 2/src/leistung/EditGameController.java
+++ Prüfungsleistung 2/src/leistung/EditGameController.java
@@ -1,5 +1,39 @@
 package leistung;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
+
 public class EditGameController {
 
+	
+	@FXML Label newGameTitle;
+	@FXML CheckBox editGameFinishedInput;
+	@FXML Label editGameGameFinished;
+	@FXML RadioButton newGameSystemInputMG;
+	@FXML Label newGameGenre;
+	@FXML Label newGameReleaseDate;
+	@FXML Label editGameSystem;
+	@FXML Label newGameOS;
+	@FXML Label newGameCS;
+	@FXML TextField newGameInputTitle;
+	@FXML ComboBox<Genre> newGameInputGenre;
+	@FXML TextField newGameInputReleaseDate;
+	@FXML RadioButton newGameSystemInputConsole;
+	@FXML RadioButton newGameSystemInputPC;
+	@FXML Button newGameEnter;
+	@FXML Button newGameQuit;
+
+	@FXML public void newGameQuit(ActionEvent event) {}
+
+	@FXML public void newGameEnter(ActionEvent event) {}
+	
+	
+	
+	
 }
